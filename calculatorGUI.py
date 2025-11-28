9d5b07cf16246b7b975976bd03d78aa18540d901
Amirhossein Jangi <amhjangi@gmail.com>
2025-11-28 18:18:47 +0330

    import library and sin and tan

---
+++ calculatorGUI.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+import math
+
+# --- توابع مثلثاتی بر اساس درجه (نه رادیان) ---
+def deg_sin(x):
+    return math.sin(math.radians(x))
+
+def deg_tan(x):
+    return math.tan(math.radians(x))
+
